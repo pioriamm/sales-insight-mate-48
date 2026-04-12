44a2407397d2f60e8f1918cd4af28fbd40625d6e
marcelo <pioriam@gmail.com>
2026-04-12 14:24:01 -0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'controller/sales_controller.dart';
+import 'firebase_options.dart';
 import 'view/sales_dashboard_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox<dynamic>(SalesController.costCatalogBoxName);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ChangeNotifierProvider(
