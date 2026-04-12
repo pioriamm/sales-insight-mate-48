e6418cb00b20dc9f9046eff7e0c8b1aa6ede8558
Pioriam <pioriam@gmail.com>
2026-04-12 15:19:39 -0300

    fix: remover blocos duplicados e referências Hive do SalesController

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'controller/sales_controller.dart';
 import 'view/sales_dashboard_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  final controller = SalesController();
+  await controller.init();
+
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => SalesController(),
+    ChangeNotifierProvider.value(
+      value: controller,
       child: const SalesInsightApp(),
     ),
   );
